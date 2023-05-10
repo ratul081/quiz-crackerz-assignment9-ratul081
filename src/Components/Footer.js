40ac8dd57175f6466c0ec9f4db6362c5994fb076
ratul081 <ratullinux77@gmail.com>
2023-05-10 18:04:42 +0600

    Full website Done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='text-center bg-slate-900 pt-10 text-white'>
       <h1 className='text-3xl lg:text-5xl pb-4'>Way to Go Developer</h1>
-      <p className='text-base lg:text-xl pt-4'>This is a very simple example of interview questions. Hope you wil able to crack i</p>
+      <p className='text-base lg:text-xl px-4'>This is a very simple example of interview questions. Hope you wil able to crack it.</p>
       <p className="text-base lg:text-xl py-4">Copyright 2023 News Today</p>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,10 +7,11 @@ const Header = () => {
       <div className="flex-1">
         <NavLink to='/' className="btn btn-ghost normal-case text-xl md:text-xl lg:text-2xl">Quiz World</NavLink>
       </div>
-      <div className="flex gap-2 space-x-3 text-lg lg:text-2xl md:text-xl mr-6">
+      <div className="flex gap-2 space-x-3 text-sm lg:text-2xl md:text-xl mr-6">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/result" >Result</NavLink>
         <NavLink to="/blog" >Blog</NavLink>
+        <NavLink to="/about" >About</NavLink>
       </div>
     </div>
   );
