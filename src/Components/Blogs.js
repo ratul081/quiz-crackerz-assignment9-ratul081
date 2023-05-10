100c16523839c69d9e8889547feb7b69d06c36dd
ratul081 <ratullinux77@gmail.com>
2023-05-10 15:55:48 +0600

    Website Done

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -9,15 +9,15 @@ const Blogs = () => {
         </div>
         <div className='m-3 p-3'>
           <h1 className='font-bold text-3xl mb-2'>Question-1: What is the purpose of React Router?</h1>
-          <p className='text-xl' ><span className='font-semibold ' >Answer: </span>  React Router is a standard library system built on top of the React and used to create routing in the React application using React Router Package. It provides the synchronous URL on the browser with data that will be displayed on the web page. It maintains the standard structure and behavior of the application and mainly used for developing single page web applications.</p>
+          <p className='text-xl' ><span className='font-semibold' >Answer: </span> React Router is a standard library system built on top of the React and used to create routing in the React application using React Router Package. It provides the synchronous URL on the browser with data that will be displayed on the web page. It maintains the standard structure and behavior of the application and mainly used for developing single page web applications.</p>
         </div>
         <div className='m-3 p-3 mt-4'>
           <h1 className='font-bold text-3xl mb-2'>Question-2: How does context API works?</h1>
-          <p className='text-xl' ><span className='font-semibold ' >Answer: </span>  Context api  pass data from parent to children nested deep down the component tree directly,It is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.</p>
+          <p className='text-xl'><span className='font-semibold'>Answer: </span> Context api  pass data from parent to children nested deep down the component tree directly,It is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.</p>
         </div>
         <div className='m-3 p-3 mt-4'>
           <h1 className='font-bold text-3xl mb-2'>Question-3: What is useRef Hook?</h1>
-          <p className='text-xl' ><span className='font-semibold ' >Answer: </span>  Ref means just reference, so it can be a reference to anything (DOM node, Javascript value, etc). The useRef hook returns a mutable object which doesn’t re-render the component when it’s changed. Think it like useState, but unlike useState, doesn’t trigger re-render of the component. The object that useRef returns have a current property that can hold any modifiable value</p>
+          <p className='text-xl'><span className='font-semibold' >Answer: </span> Ref means just reference, so it can be a reference to anything (DOM node, Javascript value, etc). The useRef hook returns a mutable object which doesn’t re-render the component when it’s changed. Think it like useState, but unlike useState, doesn’t trigger re-render of the component. The object that useRef returns have a current property that can hold any modifiable value</p>
         </div>
       </div>
     </div>
